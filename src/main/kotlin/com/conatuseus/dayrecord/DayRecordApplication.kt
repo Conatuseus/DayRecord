2eac4da85cfd3a862451945c7752379259f92951
MK S <conatuseus@gmail.com>
2020-06-05 17:23:31 +0900

    first commit

---
+++ src/main/kotlin/com/conatuseus/dayrecord/DayRecordApplication.kt
@@ -0,0 +1,11 @@
+package com.conatuseus.dayrecord
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DayRecordApplication
+
+fun main(args: Array<String>) {
+    runApplication<DayRecordApplication>(*args)
+}
---
+++ src/test/kotlin/com/conatuseus/dayrecord/SampleTest.kt
@@ -0,0 +1,13 @@
+package com.conatuseus.dayrecord
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+
+    @Test
+    fun sample() {
+        val sample = 1
+        assertEquals(sample, 1)
+    }
+}
